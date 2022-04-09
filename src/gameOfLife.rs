505fb3fdf8e33184a0e6c7e9274ef92d4e5d17f9
Federico Martinez <fedemartinezdev@gmail.com>
2022-04-09 09:14:59 +0200

    Rename main

---
+++ src/gameOfLife.rs
@@ -0,0 +1,9 @@
+pub struct GameOfLife{
+    world: Vec<Vec<char>>
+}
+
+impl GameOfLife{
+    pub fn sayHello(){
+        print!('Hello');
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::env::{args, Args};
+
+fn calculate(op1: f32, op: char, op2:f32) -> f32{
+    match op{
+        '+' => op1 + op2,
+        '-' => op1 - op2,
+        '*' => op1 * op2,
+        '/' => op1 / op2,
+        _ => panic!("Invalid operator")
+    }
+}
+
+fn main(){
+    let mut args: Args = args();
+    let operand1 = args.nth(1).unwrap();
+    let operator = args.nth(0).unwrap().chars().next().unwrap();
+    let operand2 = args.nth(0).unwrap();
+
+    let operand1f = operand1.parse::<f32>().unwrap();
+    let operand2f = operand2.parse::<f32>().unwrap();
+
+    let result = calculate(operand1f, operator, operand2f);
+
+    print!("{}{}{}={}", operand1f, operator, operand2f, result);
 }
